e2f5ded62ecdc974a599a6f1f9bd52b72c7b35e7
RiccardoAntonio <riccardoantonio55@gmail.com>
2025-08-23 15:51:49 +0200

    views

--- app/src/main/java/com/example/yu_gi_db/MainActivity.kt
+++ app/src/main/java/com/example/yu_gi_db/MainActivity.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.yu_gi_db.ui.theme.YuGiDBTheme
 import com.example.yu_gi_db.viewmodel.YuGiViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.yu_gi_db.views.MainScreen
+import com.example.yu_gi_db.views.SplashScreen
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -22,13 +28,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var splash = rememberSaveable { mutableStateOf(true) }
             YuGiDBTheme {
                 val viewModel = hiltViewModel<YuGiViewModel>()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                LaunchedEffect(Unit) {
+                    delay(2500)
+                    splash.value = false
+                }
+                if (splash.value) {
+                    SplashScreen()
+                }
+                else {
+                    MainScreen()
+
                 }
             }
         }
---
+++ app/src/main/java/com/example/yu_gi_db/views/Views.kt
@@ -0,0 +1,84 @@
+package com.example.yu_gi_db.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.yu_gi_db.R
+
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Scaffold { innerPadding -> // Use Scaffold for basic screen structure
+        Box(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            // You can replace this with your actual splash screen content
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background), // Example: Using app icon
+                contentDescription = "test1", // Add a string resource for this
+                // modifier = Modifier.fillMaxSize(),
+
+            )
+            // Or a Text:
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.app_name)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text ="test2") // Add a string resource for this
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    // AppTheme {
+    MainScreen()
+    // }
+}
